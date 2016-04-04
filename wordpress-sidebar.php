0c7c8d6ff1c6ba3cdc7bd0cfe24145002988b77b
Rangoly Jain <hirangoly@gmail.com>
2016-04-04 15:45:37 -0700

    Update wordpress-sidebar.php

--- wordpress-sidebar.php
+++ wordpress-sidebar.php
@@ -144,7 +144,7 @@
 
 		<!--  Facebook Fan Start-->
 		<div style="background-color: #FFFFFF;margin-top:15px;"> 
-			<iframe src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fpages%2FeZdia-LEARN-SHARE-ADVISE%2F189152047640&amp;width=292&amp;colorscheme=light&amp;connections=10&amp;stream=true&amp;header=true&amp;height=587" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:292px; height:587px;" allowTransparency="true">
+			<iframe src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fpages%2Fabcd-kklf-sdfs-ADVISE%2F189152047640&amp;width=292&amp;colorscheme=light&amp;connections=10&amp;stream=true&amp;header=true&amp;height=587" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:292px; height:587px;" allowTransparency="true">
 			</iframe>
 		</div>
 
